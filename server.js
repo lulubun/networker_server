4b549587862e12f90005bf15ea1d92ee10306637
lulubun <laurapetitt@gmail.com>
2017-05-09 21:10:18 -0400

    combined all in server

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const morgan = require('morgan');
 const {PORT, DATABASE_URL} = require('./config');
 const {ContactModel} = require('./models');
 const {User} = require('./userModels');
-const {router: userRouter} = require('./router');
+//const {router: userRouter} = require('./router');
+const jsonParser = require('body-parser').json();
+const LocalStrategy = require('passport-local');
+
+
 
 const app = express();
 
@@ -415,8 +419,110 @@ const jsonParser = require('body-parser').json();
 //     res.status(500).json({message: 'Internal server error'})
 //   });
 // });
+const router = express.Router();
 
-app.use('./router', userRouter);
+router.use(jsonParser);
+
+
+// NB: at time of writing, passport uses callbacks, not promises
+const localStrategy = new LocalStrategy((username, password, callback) => {
+  let user;
+  User
+    .findOne({username: username})
+    .exec()
+    .then(_user => {
+      console.log(_user);
+      user = _user;
+      if (!user) {
+        return callback(null, false, {message: 'Incorrect username'});
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return callback(null, false, {message: 'Incorrect password'});
+      }
+      else {
+        return callback(null, user)
+      }
+    });
+});
+
+passport.use(localStrategy);
+router.use(passport.initialize());
+
+
+//create a new user
+router.post('/create', (req, res) => {
+
+  let {username, password, firstName, lastName} = req.body;
+
+  if (typeof username !== 'string') {
+    return res.status(422).json({message: 'Incorrect field type: username'});
+  }
+
+  username = username.trim();
+
+  if (username === '') {
+    return res.status(422).json({message: 'Incorrect field length: username'});
+  }
+
+  if (!(password)) {
+    return res.status(422).json({message: 'Missing field: password'});
+  }
+
+  if (typeof password !== 'string') {
+    return res.status(422).json({message: 'Incorrect field type: password'});
+  }
+
+  password = password.trim();
+
+  if (password === '') {
+    return res.status(422).json({message: 'Incorrect field length: password'});
+  }
+
+  // check for existing user
+  User
+    .find({username})
+    .count()
+    .exec()
+    .then(count => {
+      if (count > 0) {
+        return res.status(422).json({message: 'username already taken'});
+      }
+      return User.hashPassword(password)
+    })
+    .then(hash => {
+      return User
+        .create({
+          username: username,
+          password: hash,
+          firstName: firstName,
+          lastName: lastName
+        })
+    })
+    .then(user => {
+      return res.status(201).json(user.apiRepr());
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+
+//validate a user
+router.post('/me',
+  passport.authenticate('local', {session: false}),
+  (req, res) => res.json({user: req.user.apiRepr()})
+);
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.use(router);
+  //'./router', userRouter);
 
 
 module.exports = {runServer, app, closeServer};
